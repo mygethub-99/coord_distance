efdd25a0ce9e099df5a7cb7ae33ef2d8003ff3ac
mygethub99 <tomakeitcount@gmail.com>
2018-01-18 06:31:39 -0600

    Cleaned up code

--- process_psap.py
+++ process_psap.py
@@ -2,16 +2,22 @@
 
 
 def makepsap():
-    filename2 = 'psap_list.txt'
-    fopen2 = open(filename2, 'r')
+    fopen2 = open('psap_list.txt')
     cont2 = fopen2.readlines()
     psap_dict = {}
     fopen2.close()
-
-    for line in range(1, len(cont2)):
-        splitsit=cont2[line].split()
-        psap_dict[splitsit[0]]=splitsit[1],splitsit[2]
+    
+    try:
+        for line in range(1, len(cont2)):
+            splitsit=cont2[line].split()
+            psap_dict[splitsit[0]]=splitsit[1],splitsit[2]
+    
+    except:
+        for line in range(1, (len(cont2))-1):
+            splitsit=cont2[line].split()
+            psap_dict[splitsit[0]]=splitsit[1],splitsit[2]
     
     return (psap_dict)
+    
 
 makepsap()
